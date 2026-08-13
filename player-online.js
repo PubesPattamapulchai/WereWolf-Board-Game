75319d0fb0786bc095a61cd312c31f7e62e0920a
Shibaman <pattamapulchai.pubes@gmail.com>
2026-08-13 16:18:38 +0700

    Bust player module cache for Firebase connection fix

--- player-online.js
+++ player-online.js
@@ -1,1 +1,3 @@
-import "./player-v2.js";
+// Cache-busted player entrypoint. Bump PLAYER_BUILD whenever player-v2.js changes.
+const PLAYER_BUILD = "20260813-1616-firebase-fix";
+await import(`./player-v2.js?v=${PLAYER_BUILD}`);
