549aaa523df6b469e008b0705ca28da6e3ca3a7e
Shibaman <pattamapulchai.pubes@gmail.com>
2026-08-14 08:09:47 +0700

    Load audited vote flow on top of host night engine

--- moderator-online.js
+++ moderator-online.js
@@ -1,1 +1,3 @@
-import "./host-player-v2.js";
+// Host entrypoint v3: keep the automatic night engine and layer the audited day-vote flow on top.
+await import("./host-player-v2.js?v=20260814-audit1");
+await import("./vote-flow-v3.js?v=20260814-audit1");
